76d8a38a201ba90365629dc17d8eea7aae8a4a35
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-05-11 04:49:32 -0500

    changes

--- Actividades/pruebas.py
+++ Actividades/pruebas.py
@@ -1,5 +1,16 @@
+# 2D lists
 a = ["a", "b", "c"], [1, 2, 3, 4]
 print(a[0][-2])
 
+# para entender parte de la sesion6
 numero = "1234"
 print("this shit: ", int(numero[1]))
+
+# esto
+a = 5
+a = a + 5
+print(a)
+# es lo mismo q esto
+b = 5
+b += 5  # solo q aqui esta abreviado
+print(b)
