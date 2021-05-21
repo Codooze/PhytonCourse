d5f4cfd88d4a83fe11707eadfc2c59c1441cc629
jeison-AK <50089525+jeison-AK@users.noreply.github.com>
2021-05-21 09:55:44 -0500

    sesion9

--- Actividades/pruebas/funtions.py
+++ Actividades/pruebas/funtions.py
@@ -39,15 +39,6 @@ def trip_welcome(destination="California"):
 print(budget)
 trip_welcome()
 
-# primero para que te des cuenta si hay variables que estan conectadas puedes darle click a alguna y las variables con el mismo nombre se
-# se subrayaran por ejemplo si seleccinas la variables abc veras que la funcion tiene acceso a esta
-# ahora si seleccionas la variable "a" te daras cuenta que la funcion no tiene acceso a esta variable en cambio phyton lo que hace es crear
-# una nueva variable "a" que no esta conectada a la anterior, con esto ya ves que no puedes modificarla desde la funcion pero si haces esto
-# asigna_valor_de_a = a   sí podras asignarle el valor, ahi ves que si le das click a "a" se sombrean ambas
-
-# pd: ese es solo un ejeplo para entender una parte especifica del funcionamiento de las funciones, si lo que quieres asimilar
-# qué es eso de funciones de pasarle parametros para eso tengo una analogia pero creo que es mejor explicarla en un meet
-
 print("////////////////////////////")
 # para eliminar el salto de linea
 coleciones = "jeison"
--- Actividades/sesion9.py
+++ Actividades/sesion9.py
@@ -9,6 +9,7 @@ En la semana uno exploramos el tipo de dato string (una cadena de caracteres).
 Los caracteres dentro de este tipo de dato en Python puede ser recorridos con la posicion en la que se encuentran dentro de la cadena. Veamos un ejemplo:
 """
 
+
 def ejemplo1():
     palabra = "Prueba"
     longitud = len(palabra)
@@ -21,33 +22,99 @@ def ejemplo1():
     for x in "Prueba":
         print(x)
 
-ejemplo1()
-
+# ejemplo1()
 
 
 def actividad1():
     print("actividad1")
+    num = int(input("fibo: "))
+    n_anterior = 0
+    a = 1
+    r = 0
+    for i in range(num):
+        n_anterior = a+r
+        a = r
+        r = n_anterior
+        print(f"{r}", end=" ")
     # Vamos a realizar un algoritmo que nos calcule la serie de Fibonacci.
-    # La serie o secuencia de Fibonacci comienza con los números 0 y 1 y 1, y a partir de allí es posible calcular el siguiente valor como la suma de los dos valores anteriores. 
+    # La serie o secuencia de Fibonacci comienza con los números 0 y 1 y 1, y a partir de allí es posible calcular el siguiente valor como la suma de los dos valores anteriores.
     # Por ejemplo, 1+1=2, 1+2=3, 2+3=5, etc. Así, los primeros números de la secuencia son: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
     # Creemos un programa que a partir de un número N ingresado, imprima los primeros N números de la serie de Fibonacci.
- 
-#actividad1()
+
+# actividad1()
+
 
 def actividad2():
     print("actividad2")
-    #Escribe el código usando break que reciba una palabra e imprima el número de letras que tiene y las letras hasta que, o bien termine la palabra o encuentre la letra a. .
+    palabra = input("palabra: ")
+    for i in palabra:
+        if i == "a":
+            break
+        else:
+            print(i, end=" ")
 
-#actividad2()
+    # Escribe el código usando break que reciba una palabra e imprima el número de letras que tiene y las letras hasta que, o bien termine la palabra o encuentre la letra a. .
+
+
+# actividad2()
+
 
 def actividad3():
     print("actividad3")
-    #Escribe un algoritmo que lea 10 números e imprima cuantos son positivos, cuantos negativos y cuantos neutros(0).
+    num = input("ingresa 10 numeros: ")
+    num_len = len(num)
+    n_has_minus = num_len
+    n_positivos = 0
+    n_negativos = 0
+    n_neutros = 0
+    if num.count("-"):
+        n_has_minus -= num.count("-")
+    while n_has_minus != 10:
+        print("I said 10 numbers: ")
+        print(num_len)
+        num = input()
+        num_len = len(num)
+        n_has_minus = num_len
+        if num.count("-"):
+            n_has_minus -= num.count("-")
+    for i in range(num_len):
+        print("doing for")
+        if num[i] == "-":
+            n_negativos += 1
+            n_positivos -= 1
+            continue
+        if int(num[i]) > 0:
+            n_positivos += 1
+        elif int(num[i]) < 0:
+            n_negativos += 1
+        else:
+            n_neutros += 1
+        print(num[i])
+    print(n_positivos, n_negativos, n_neutros)
+    # Escribe un algoritmo que lea 10 números e imprima cuantos son positivos, cuantos negativos y cuantos neutros(0).
 
-#actividad3()
+
+actividad3()
+
+# string = "123132-1"
+# string_len = len(string)
+# n_negativos = 0
+# for i in range(string_len):
+#     # print(string[i], end= "")
+#     n_to_evaluate = string[i]
+#     print(n_to_evaluate)
+#     if n_to_evaluate == "-":
+#         n_negativos +=1
+# print("negativos: ", n_negativos)
+
+# to_int = int(string[i])
+# print(to_int)
+
+# print(int(str(-5)))
+
 
 def actividad4():
     print("actividad4")
-    #Usando tanto while como for, escribe el codigo que pida números al usuario hasta que este ingrese -1 y que retorne el factorial de cada número ingresado usando un ciclo Para (For).
-    
-#actividad4()
+    # Usando tanto while como for, escribe el codigo que pida números al usuario hasta que este ingrese -1 y que retorne el factorial de cada número ingresado usando un ciclo Para (For).
+
+# actividad4()
